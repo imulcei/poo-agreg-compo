dd540f5218aa78fffb7e154a8b91588e32b88906
Vincenzo Tinebra <vince.great66@gmail.com>
2025-05-06 09:46:28 +0200

    Correction

--- batiment/src/main/java/fr/afpa/App.java
+++ batiment/src/main/java/fr/afpa/App.java
@@ -5,6 +5,8 @@ import fr.afpa.model.bo.Meuble;
 import fr.afpa.model.bo.Piece;
 
 public class App {
+
+    // TODO Gérer les messages avec des Exceptions au lieu de println()
     public static void main(String[] args) {
         Meuble table = new Meuble(150, 100, 90, "Table");
         Meuble chaise = new Meuble(80, 120, 80, "Chaise");
